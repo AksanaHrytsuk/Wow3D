a855541010b390c3c906af9989e4e2daeaa47130
Oksana <kroshkaksju@gmail.com>
2020-05-21 21:19:29 +0300

    added 3dObjects, physicMaterial, meshRenderMaterial, isTrigger, bouncy, addForce

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public float jumpForce;
+    public float speed;
+    
+    private new Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+           // rigidbody.AddTorque(Vector3.forward * jumpForce, ForceMode.Impulse);
+        }
+
+        float inputX = Input.GetAxis("Horizontal");// влево вправо
+        float inputZ = Input.GetAxis("Vertical"); //вперед назад
+        
+        rigidbody.AddForce(new Vector3(inputX, 0, inputZ) * speed);
+    }
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private MeshRenderer _meshRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"));
+        {
+            MeshRenderer ballRenderer = collision.gameObject.GetComponent<MeshRenderer>();
+            Material ballMaterial = ballRenderer.material;
+            _meshRenderer.material = ballMaterial;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _meshRenderer.material.color = Color.green;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _meshRenderer.material.color = Color.yellow;
+        }
+    }
+}
