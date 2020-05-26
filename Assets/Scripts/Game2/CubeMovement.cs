e5748bef69a06187db6e18152b6d93ab26d6f40c
Oksana <kroshkaksju@gmail.com>
2020-05-26 21:18:19 +0300

    Spikes

--- Assets/Scripts/Game2/CubeMovement.cs
+++ Assets/Scripts/Game2/CubeMovement.cs
@@ -6,18 +6,25 @@ using DG.Tweening;
 public class CubeMovement : MonoBehaviour
 {
     [SerializeField] private float moveTime = 0.5f;
+
+    [SerializeField] private float jumpPower = 0.5f;
+
+    private bool allowInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+        allowInput = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!allowInput)
+        {
+            return;
+        }
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            // Vector3 newPosition = transform.position + new Vector3(0, 0, 1);
+            // Vector3 newPosition = transform.position + new Vector3(0, 0, 1); движение по Z(синяя стрелка)
             Vector3 newPosition = transform.position + Vector3.forward;
             MoveTo(newPosition);
         }
@@ -41,7 +48,19 @@ public class CubeMovement : MonoBehaviour
 
     void MoveTo(Vector3 newPosition)
     {
-        transform.DOMove(newPosition, moveTime)
-            .SetEase(Ease.OutElastic);
+        // Debug.DrawRay(newPosition,Vector3.down,Color.green, 2f); показать луч
+        if (Physics.Raycast(newPosition, Vector3.down))
+        
+        //transform.DOMove(newPosition, moveTime) .SetEase(Ease.OutElastic); движение без прыжков
+        allowInput = false;
+        transform.DOJump(newPosition, jumpPower, 1, moveTime).OnComplete(ResetInput);
+        
+        // Invoke(nameof(ResetInput), moveTime);
+           
+    }
+
+    void ResetInput()
+    {
+        allowInput = true;
     }
 }
---
+++ Assets/Scripts/Game2/Spikes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] private float moveTime = 1;
+
+    [SerializeField] private float waitTime = 1;
+
+    [SerializeField] private float maxYValue = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator Movement()
+    {
+        while (true)
+        {
+            transform.DOMoveY(maxYValue, moveTime);
+            yield return new WaitForSeconds(moveTime + waitTime);
+            transform.DOMoveY(0, moveTime);
+            yield return new WaitForSeconds(moveTime + waitTime);
+        }
+    }
+}
