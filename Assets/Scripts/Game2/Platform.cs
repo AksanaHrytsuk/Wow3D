b4d82966c594a7cbefb437583888a50ae2afc6a1
Oksana <kroshkaksju@gmail.com>
2020-06-09 16:40:21 +0300

    Platform with diamonds

---
+++ Assets/Scripts/Game2/Platform.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Platform : MonoBehaviour
+{
+    public bool forward;
+
+    public bool back;
+
+    public bool left;
+
+    public bool right;
+    public int units;
+
+    private float _targetRight;
+    private float _targetLeft;
+    private float _targetForward;
+    private float _targetBack;
+
+    private void Start()
+    {
+        // сохранить состояние
+        _targetRight = transform.position.x + units;
+        _targetLeft = transform.position.x - units;
+        _targetForward = transform.position.z + units;
+        _targetBack = transform.position.z - units;
+
+    }
+
+    public void Move()
+    {
+        if (forward)
+        {
+            transform.DOMoveZ(_targetForward, 1, false);
+        }
+
+        if (back)
+        {
+            transform.DOMoveZ(_targetBack, 1, false);
+        }
+
+        if (left)
+        {
+            transform.DOMoveX(_targetLeft, 1, false);
+        }
+
+        if (right)
+        {
+            // Vector3 target = new Vector3(transform.position.x + 0.8f , transform.position.y, transform.forward.z);
+            transform.DOMoveX (_targetRight, 1, false);
+        }
+    }
+
+
+    // void ChangePosition(PlatformStates newPlatformPosition) 
+    // {
+    //     movemenPlatform = newPlatformPosition;
+    //     
+    //     switch (movemenPlatform) 
+    //     {
+    //         case PlatformStates.Forward:
+    //            // _aiDestinationSetter.followPlayer = true;
+    //           
+    //             break;
+    //         case PlatformStates.Back:
+    //            // _aiDestinationSetter.followPlayer = false;
+    //            
+    //             break;
+    //         case PlatformStates.Left:
+    //         {
+    //         }
+    //             break;
+    //         case PlatformStates.Right:
+    //             
+    //             break;
+    //     } 
+    // }
+
+    // void MoveTo(Vector3 newPosition)
+    // {
+    //     transform.DOMove(newPosition, 1).SetEase(Ease.InBounce);
+    // }
+}
---
+++ Assets/Scripts/Game2/PullPlatformButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullPlatformButton : MonoBehaviour
+{
+    public Platform _platform;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _platform.Move();
+        }
+    }
+}
