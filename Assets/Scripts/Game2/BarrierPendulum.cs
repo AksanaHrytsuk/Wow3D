4dc6bf7b4da72fd1fc2c707264d42547581b7f7c
Oksana <kroshkaksju@gmail.com>
2020-06-15 23:51:16 +0300

    BarrierPendulum

--- Assets/Scripts/Game2/BarrierPendulum.cs
+++ Assets/Scripts/Game2/BarrierPendulum.cs
@@ -5,26 +5,40 @@ using UnityEngine;
 using DG.Tweening;
 public class BarrierPendulum : MonoBehaviour
 {
-    [Header("Components")] private CubeMovement _cube;
-
-    private Sequence _sequence;
+    [Header("Config parameters")]
+    [SerializeField] private float moveTime;
+    [SerializeField] private float waitTimeUp;
+    [Header("Movement parameters")]
+    [SerializeField] private AnimationCurve movementEase1;
+    [SerializeField] private AnimationCurve movementEase2;
     
+    private CubeMovement _cube;
+    private Sequence _sequence;
+    Vector3 _moveLeft = new Vector3(0, 0, 120);
+    Vector3 _moveRight = new Vector3(0, 0, -120);
+    Vector3 _moveCenter = new Vector3(0, 0, 0);
+    
+
+
     void Start()
     {
         _cube = FindObjectOfType<CubeMovement>();
+        
+        _sequence = DOTween.Sequence();
+        _sequence.Append(transform.DORotate(_moveRight, moveTime).SetEase(movementEase1));
+        _sequence.AppendInterval(waitTimeUp);
+        _sequence.Append(transform.DORotate(_moveCenter, moveTime).SetEase(movementEase2));
+        _sequence.Append(transform.DORotate(_moveLeft, moveTime).SetEase(movementEase1));
+        _sequence.AppendInterval(waitTimeUp);
+        _sequence.Append(transform.DORotate(_moveCenter, moveTime).SetEase(movementEase2));
+        _sequence.SetLoops(-1, LoopType.Restart);
     }
 
-    public void OnCollisionEnter(Collision other)
+    public void OnTriggerEnter(Collider other)
     {
-        if (other.collider.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             _cube.Die();
         }
-    }
-
-    void BarrierPendulumMovement()
-    {
-        _sequence = DOTween.Sequence();
-        
     }
 }
