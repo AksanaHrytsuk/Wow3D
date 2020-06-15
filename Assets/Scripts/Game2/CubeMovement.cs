b52fb867289eb39e9554938b1ad15b83b1178271
Oksana <kroshkaksju@gmail.com>
2020-06-15 03:27:03 +0300

    layer Mask for burrier

--- Assets/Scripts/Game2/CubeMovement.cs
+++ Assets/Scripts/Game2/CubeMovement.cs
@@ -7,13 +7,15 @@ public class CubeMovement : MonoBehaviour
     [Header("Config parametrs")]
     [SerializeField] float moveTime;
     [SerializeField] float jumpPower;
-    
     [SerializeField] private float reloadLevelDelay = 1;
+    
+    [Header("Effects")]
     [SerializeField] private GameObject deathEffect;
 
     [Header("Sounds")] [SerializeField] private AudioClip deathSound;
     
     private Rigidbody rigidbody;
+    private Barrier _barrier;
     
     bool allowInput;
 
@@ -58,7 +60,8 @@ public class CubeMovement : MonoBehaviour
     }
 
     private void Awake()
-    { 
+    {
+        _barrier = FindObjectOfType<Barrier>();
         rigidbody = GetComponent<Rigidbody>();
     }
 
@@ -79,22 +82,22 @@ public class CubeMovement : MonoBehaviour
         {
             //new Vector3(0, 0, 1) = Vector3.forward
             Vector3 newPosition = transform.position + Vector3.forward;
-            MoveTo(newPosition);
+            MoveTo(newPosition, Vector3.forward);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Vector3 newPosition = transform.position + Vector3.back;
-            MoveTo(newPosition);
+            MoveTo(newPosition, Vector3.back);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             Vector3 newPosition = transform.position + Vector3.left;
-            MoveTo(newPosition);
+            MoveTo(newPosition,Vector3.left);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             Vector3 newPosition = transform.position + Vector3.right;
-            MoveTo(newPosition);
+            MoveTo(newPosition, Vector3.right);
         }
     }
 
@@ -102,36 +105,39 @@ public class CubeMovement : MonoBehaviour
     {
         if (!allowInput) { return; }
         Vector3 newPosition = transform.position + Vector3.forward;
-        MoveTo(newPosition);
+        MoveTo(newPosition, Vector3.forward);
     }
 
     public void MoveBack()
     {
         if (!allowInput) { return; }
         Vector3 newPosition = transform.position + Vector3.back;
-        MoveTo(newPosition);
+        MoveTo(newPosition, Vector3.back);
     }
 
     public void MoveLeft()
     {
         if (!allowInput) { return; }
         Vector3 newPosition = transform.position + Vector3.left;
-        MoveTo(newPosition);
+        MoveTo(newPosition, Vector3.left);
     }
 
     public void MoveRight()
     {
         if (!allowInput) { return; }
         Vector3 newPosition = transform.position + Vector3.right;
-        MoveTo(newPosition);
+        MoveTo(newPosition, Vector3.right);
     }
 
+
     //Движение MoveTo в новую позицию newPosition
-    void MoveTo(Vector3 newPosition)
+    void MoveTo(Vector3 newPosition , Vector3 direction)
     {
+        LayerMask layerMask = LayerMask.GetMask($"Barrier");
+
         //Debug.DrawRay(newPosition, Vector3.down, Color.green, 2f);
         // Raycast проверяет, есть ли какой-то объект в newPosition в направлении Vector3.dowБ если да, то выполняем движ.
-        if (Physics.Raycast(newPosition, Vector3.down, 1f))
+        if (Physics.Raycast(newPosition, Vector3.down, 1f) && !Physics.Raycast(transform.position, direction: direction, maxDistance:1, layerMask))
         {
             // запретить ещё одно движение во время начатого движения
             allowInput = false;
